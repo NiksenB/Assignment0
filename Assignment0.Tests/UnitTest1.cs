61efd3578489220059d64ae811bc52a0fdbe71d8
NiksenB <90034452+NiksenB@users.noreply.github.com>
2021-09-06 21:14:31 +0200

    Branch2 (#2)
    * add things
    
    * get complete answer success
    
    * supercommit1
    
    Co-authored-by: Nikoline <nibu@itu.dk>

--- Assignment0.Tests/UnitTest1.cs
+++ Assignment0.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Xunit;
 
 namespace Assignment0.Tests
@@ -6,9 +7,69 @@ namespace Assignment0.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void test_true_isLeapYear_2020()
         {
+            //Arrange
+            var input = 2020;
+            
+            //Act
+            var output = Program.IsLeapYear(input);
 
+            //Assert
+            Assert.Equal(true, output);
         }
+
+        [Fact]
+        public void test_true_isLeapYear_44bc()
+        {
+            //Arrange
+            var input = -44;
+            
+            //Act
+            var output = Program.IsLeapYear(input);
+
+            //Assert
+            Assert.Equal(true, output);
+        }
+
+        [Fact]
+        public void test_false_isLeapYear_2019()
+        {
+            //Arrange
+            var input = 2019;
+            
+            //Act
+            var output = Program.IsLeapYear(input);
+
+            //Assert
+            Assert.Equal(false, output);
+        }
+
+        [Fact]
+        public void test_false_isLeapYear_2100()
+        {
+            //Arrange
+            var input = 2100;
+            
+            //Act
+            var output = Program.IsLeapYear(input);
+
+            //Assert
+            Assert.Equal(false, output);
+        }
+
+        [Fact]
+        public void test_true_isLeapYear_2400()
+        {
+            //Arrange
+            var input = 2400;
+            
+            //Act
+            var output = Program.IsLeapYear(input);
+
+            //Assert
+            Assert.Equal(true, output);
+        }
+        
     }
 }
