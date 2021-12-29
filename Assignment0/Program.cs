2dbb1e9de03fcb796b47e7f0c71512363c2c1f29
NiksenB <90034452+NiksenB@users.noreply.github.com>
2021-12-29 19:58:46 +0100

    update to net6

--- Assignment0/Program.cs
+++ Assignment0/Program.cs
@@ -1,38 +1,38 @@
 using System;
 
-namespace Assignment0
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            try {
-                var input = Int32.Parse(Console.ReadLine());
-                if (IsLeapYear(input)){
-                    Console.WriteLine("yay");
-                } else {
-                    Console.WriteLine("nay");
-                }
-            } catch (FormatException) {
-                Console.WriteLine("Exception! Please write a number...");
-            }
-        }
+namespace Assignment0;
 
-        public static bool IsLeapYear(int year) 
-        {
-            if (year <= 1581) {
-                throw new InvalidInputException("The year " + year + " is too soon!");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        try {
+            var input = Int32.Parse(Console.ReadLine());
+            if (IsLeapYear(input)){
+                Console.WriteLine("yay");
             } else {
-                if (year % 4 == 0) {
-                    if (year % 100 != 0 || year % 400 == 0) {
-                        return true;
-                    } else {
-                        return false;
-                    }
+                Console.WriteLine("nay");
+            }
+        } catch (FormatException) {
+            Console.WriteLine("Exception! Please write a number...");
+        }
+    }
+
+    public static bool IsLeapYear(int year) 
+    {
+        if (year <= 1581) {
+            throw new InvalidInputException("The year " + year + " is too soon!");
+        } else {
+            if (year % 4 == 0) {
+                if (year % 100 != 0 || year % 400 == 0) {
+                    return true;
                 } else {
                     return false;
                 }
+            } else {
+                return false;
             }
         }
     }
 }
+
